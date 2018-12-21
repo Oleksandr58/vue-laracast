068ce604f99934a1c94267eaa27bb4a13160bd9d
Oleksandr58 <33155100+Oleksandr58@users.noreply.github.com>
2018-12-21 12:38:46 +0200

    Bookimed widget

--- widget.js
+++ widget.js
@@ -15,7 +15,7 @@ function bookimedWidget(obj) {
         this.headerTopWidget = this.headerWidget.querySelector('.b-w-top');
         this.headerDownWidget = this.headerWidget.querySelector('.b-w-down');
         this.contentWidget = this.widget.querySelector('.b-w-content');
-        this.reviews = this.widget.querySelectorAll('.b-w-review');
+        this.reviews = this.widget.querySelectorAll('.b-w-review') || this.contentWidget;
         this.btnMore = this.widget.querySelectorAll('div.b-w-more');
         this.preloader = this.widget.querySelector('.b-w-preloader');
         this.lastReview = this.reviews[this.reviews.length - 1];
