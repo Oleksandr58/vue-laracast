180bc516c50884170abf9289d82a72cd75adf2d5
Oleksandr58 <33155100+Oleksandr58@users.noreply.github.com>
2018-12-21 13:21:16 +0200

    Bookimed widget

--- widget.js
+++ widget.js
@@ -59,6 +59,7 @@ function bookimedWidget(obj) {
         this.setSize = function() {
             this.widget.setAttribute("style", "max-width:" + this.maxWidth + ";max-height:" + this.maxHeight);
 
+            console.log(1112);
             if (this.maxWidth < 520) {
                 console.log(111);
                  this.widget.classList.add('small');
